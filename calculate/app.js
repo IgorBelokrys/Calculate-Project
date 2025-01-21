44cdb6202479d9876b154449016512fe3ad32d0e
IgorBel <121041077+IgorBelokrys@users.noreply.github.com>
2025-01-21 20:50:20 +0300

    creating logic\part-2

--- calculate/app.js
+++ calculate/app.js
@@ -1,30 +1,68 @@
 "use strict";
 
 const interfaceFirst = document.querySelector(".interface-first");
-const interfaceSecond = document.querySelector(".interface-second");
+// const interfaceSecond = document.querySelector(".interface-second");
 
 const buttons = document.querySelectorAll(".buttons button");
 
-let currentValue = "";
-let previousValue = "";
-let operator = "";
-
-function updateDisplay() {
-  interfaceFirst.textContent = currentValue || "0";
-}
-
-function crunchingNumber(number) {
-  if (currentValue === "0" || currentValue === "") {
-    currentValue = number;
-  } else {
-    currentValue += number;
-  }
-  updateDisplay();
-}
+let currentNumber = "";
+let previousNumber = "";
+let operation = "";
 
 buttons.forEach((elem) => {
   elem.addEventListener("click", function (event) {
-    const clickButton = event.target;
-    interfaceFirst.textContent = clickButton.textContent;
+    const clickButton = event.target.closest("button");
+    console.log(`Клик по кнопке:`, clickButton);
+    if (clickButton.classList.contains("reset")) {
+      interfaceFirst.textContent = "";
+      return;
+    }
+    if (clickButton.classList.contains("delete")) {
+      if (interfaceFirst.textContent.length > 0) {
+        interfaceFirst.textContent = interfaceFirst.textContent.slice(0, -1);
+      }
+      return;
+    }
+    const buttonText = clickButton.textContent.trim();
+    interfaceFirst.textContent += buttonText;
+
+    if (
+      clickButton.classList.contains("division") ||
+      clickButton.classList.contains("multiplication") ||
+      clickButton.classList.contains("subtract") ||
+      clickButton.classList.contains("add")
+    ) {
+      previousNumber = interfaceFirst.textContent;
+      operation = clickButton.textContent.trim();
+      interfaceFirst.textContent = "";
+      console.log(previousNumber, operation);
+      return;
+    }
+
+    if (clickButton.classList.contains("equals")) {
+      console.log("Нажата кнопка =", previousNumber, operation, currentNumber);
+      currentNumber = interfaceFirst.textContent;
+      let result;
+      switch (operation) {
+        case "/":
+          result = Number(previousNumber) / Number(currentNumber);
+          break;
+        case "*":
+          result = Number(previousNumber) * Number(currentNumber);
+          break;
+        case "-":
+          result = Number(previousNumber) - Number(currentNumber);
+          break;
+        case "+":
+          result = Number(previousNumber) + Number(currentNumber);
+          break;
+        default:
+          result = currentNumber;
+      }
+      console.log("Результат:", result);
+      interfaceFirst.textContent = result;
+      previousNumber = "";
+      operation = "";
+    }
   });
 });
